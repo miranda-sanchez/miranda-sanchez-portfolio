3605f00c448fca7b39218061b58ae535adc3b89e
miranda-sanchez <121776162+miranda-sanchez@users.noreply.github.com>
2024-07-29 22:06:58 -0300

    Further changes in Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,37 +1,71 @@
 import React from "react";
+import { FiArrowUpRight } from "react-icons/fi";
 
 const Projects = () => {
   return (
     <main className="Projects">
       <h3>Selected Projects</h3>
-      <ol>
-        <li>
+      <section>
+        <article>
           Musée du Louvre
+          <span>Design & Development | 2024 | React</span>
           <div className="btn-container">
             <a href="https://louvree.netlify.app/" target="_blank">
-              <button className="btn-action">Visit</button>
+              <button className="btn-action">
+                Visit <FiArrowUpRight className="btn-arrow" />
+              </button>
             </a>
             <a
               href="https://github.com/miranda-sanchez/louvre"
               target="_blank"
             ></a>
-            <button>Code</button>
+            <button>
+              Code <FiArrowUpRight className="btn-arrow" />
+            </button>
           </div>
-        </li>
-        <li>
+        </article>
+        <article>
           Fetsi (Furniture e-commerce)
+          <span>
+            Design, Development & Illustrations | 2023 | Vainilla JavaScript
+          </span>
           <div className="btn-container">
             <a href="https://miranda-sanchez.github.io/fetzi" target="_blank">
-              <button className="btn-action">Visit</button>
+              <button className="btn-action">
+                Visit <FiArrowUpRight className="btn-arrow" />
+              </button>
             </a>
             <a
-              href="https://github.com/miranda-sanchez/louvre"
+              href="https://github.com/miranda-sanchez/fetzi"
               target="_blank"
             ></a>
-            <button>Code</button>
+            <button>
+              Code <FiArrowUpRight className="btn-arrow" />
+            </button>
           </div>
-        </li>
-      </ol>
+        </article>
+        <article>
+          Quizz App
+          <span>Design & Development | 2022 | Vainilla JavaScript</span>
+          <div className="btn-container">
+            <a
+              href="https://miranda-sanchez.github.io/quiz-app/"
+              target="_blank"
+            >
+              <button className="btn-action">
+                Visit <FiArrowUpRight className="btn-arrow" />
+              </button>
+            </a>
+            <a
+              href="https://github.com/miranda-sanchez/quiz-app/"
+              target="_blank"
+            ></a>
+            <button>
+              Code <FiArrowUpRight className="btn-arrow" />
+            </button>
+          </div>
+        </article>
+      </section>
     </main>
   );
 };
